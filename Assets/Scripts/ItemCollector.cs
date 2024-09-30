8f797b4a87469f8aa31ad4af363ae43ca2d3b4b3
summerkrd <145993794+summerkrd@users.noreply.github.com>
2024-09-30 15:32:10 +0300

    0.01

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class ItemCollector : MonoBehaviour
+{
+    [SerializeField] private Transform _handPointPosition;
+
+    private Item _item;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Item>(out Item item))
+        {
+            if (_item == null)
+            {
+                _item = item;
+                _item.transform.SetParent(_handPointPosition);
+                _item.transform.localPosition = Vector3.zero;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (_item != null)
+            {
+                UseItem();
+            }                
+        }
+    }
+
+    private void UseItem()
+    {
+        Destroy(_item.gameObject);
+        _item = null;
+    }
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,13 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 1f;
-    
+
     private CharacterController _characterController;
     private Vector3 _moveDirection = Vector3.zero;
+    private float _rotateSpeed = 5f;
+    private float _lastNonZeroXDirection = 1f;
 
     private void Awake()
     {
@@ -17,9 +17,37 @@ public class PlayerMove : MonoBehaviour
     private void Update()
     {
         _moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        //_moveDirection = transform.TransformDirection(_moveDirection);
-        //_moveDirection *= _moveSpeed;
 
         _characterController.Move(_moveDirection * _moveSpeed * Time.deltaTime);
+
+        if (_moveDirection.x != 0)
+        {
+            _lastNonZeroXDirection = Mathf.Sign(_moveDirection.x);
+        }
+
+        Rotate();
+
+        Debug.Log(_moveDirection.x);
+    }
+
+    private void Rotate()
+    {
+        Vector3 eulerAngles = transform.localEulerAngles;
+        Vector3 newEulerAngles;
+
+        if (_moveDirection.x < -0.1f)
+        {
+            newEulerAngles = new Vector3(transform.rotation.x, 180f, transform.rotation.z);
+        }
+        else if (_moveDirection.x > 0.1f)
+        {
+            newEulerAngles = new Vector3(transform.rotation.x, 0f, transform.rotation.z);
+        }
+        else
+        {
+            newEulerAngles = new Vector3(transform.rotation.x, _lastNonZeroXDirection > 0 ? 0f : 180f, transform.rotation.z);
+        }
+
+        transform.localEulerAngles = Vector3.Lerp(eulerAngles, newEulerAngles, Time.deltaTime * _rotateSpeed);
     }
 }
