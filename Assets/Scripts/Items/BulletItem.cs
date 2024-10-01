d2ca5b309fed391ce3926763aaba0af0fdffac1f
summerkrd <145993794+summerkrd@users.noreply.github.com>
2024-10-01 21:20:49 +0300

    Finish

--- Assets/Scripts/Items/BulletItem.cs
+++ Assets/Scripts/Items/BulletItem.cs
@@ -2,8 +2,23 @@ using UnityEngine;
 
 public class BulletItem : Item
 {
-    //public override void Use(GameObject owner)
-    //{
-    //    base.Use(owner);
-    //}
+    [SerializeField] private BulletPrefab _bulletPrefab;
+    [SerializeField] private float _shootForce;
+
+    private HandPoint _handPoint;
+
+    public override void Use(GameObject owner)
+    {
+        _handPoint = owner.GetComponentInChildren<HandPoint>();
+
+        base.Use(owner);
+
+        if (_handPoint != null)
+        {
+            BulletPrefab bulletPrefab = Instantiate(_bulletPrefab, _handPoint.transform.position, Quaternion.identity);
+
+            Rigidbody rigidbody = bulletPrefab.GetComponent<Rigidbody>();
+            rigidbody.AddForce(_handPoint.transform.forward * _shootForce, ForceMode.Impulse);
+        }
+    }
 }
